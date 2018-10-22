76d9f2d15054627a24a7411ad058337159f5cbe5
Horia <horia@instacarshare.com>
2018-10-22 18:07:03 +0300

    no more PIL

--- src/wsgi.py
+++ src/wsgi.py
@@ -8,7 +8,6 @@ from hmac import HMAC
 import hashlib
 from tests_queue import get_commit_pr_queue_singleton
 from commit_pr_model import CommitPrModel
-from PIL import Image
 from io import StringIO
 
 singleton = get_commit_pr_queue_singleton()
@@ -211,22 +210,6 @@ HEIGHT = 480
 
 @app.route('/<path:filename>')
 def image(filename):
-    try:
-        w = int(request.args['w'])
-        h = int(request.args['h'])
-    except (KeyError, ValueError):
-        return send_from_directory('.', filename)
-
-    try:
-        im = Image.open(filename)
-        im.thumbnail((w, h), Image.ANTIALIAS)
-        io = StringIO.StringIO()
-        im.save(io, format='JPEG')
-        return Response(io.getvalue(), mimetype='image/jpeg')
-
-    except IOError:
-        abort(404)
-
     return send_from_directory('.', filename)
 
 @app.route('/screenshots/<commit_sha>')
